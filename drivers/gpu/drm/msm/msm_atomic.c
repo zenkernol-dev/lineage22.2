0ad9bb84a548329b96c3806bf391dc0780624e25
CloudedQuartz <ravenklawasd@gmail.com>
2021-11-21 22:38:18 +0530

    drivers: gpu: nuke xiaomi smartfps
    - Unnecessary overhead
    
    Signed-off-by: CloudedQuartz <ravenklawasd@gmail.com>

--- drivers/gpu/drm/msm/msm_atomic.c
+++ drivers/gpu/drm/msm/msm_atomic.c
@@ -26,7 +26,6 @@
 #include "msm_gem.h"
 #include "msm_fence.h"
 #include "sde_trace.h"
-#include "xiaomi_frame_stat.h"
 
 #define MULTIPLE_CONN_DETECTED(x) (x > 1)
 
@@ -588,16 +587,6 @@ static void complete_commit(struct msm_commit *c)
 static void _msm_drm_commit_work_cb(struct kthread_work *work)
 {
 	struct msm_commit *c = container_of(work, typeof(*c), commit_work);
-	ktime_t start, end;
-	s64 duration;
-
-	if (!work) {
-		DRM_ERROR("%s: Invalid commit work data!\n", __func__);
-		return;
-	}
-
-	start = ktime_get();
-	frame_stat_collector(0, COMMIT_START_TS);
 	struct pm_qos_request req = {
 		.type = PM_QOS_REQ_AFFINE_CORES,
 		.cpus_affine = ATOMIC_INIT(BIT(raw_smp_processor_id()))
@@ -614,9 +603,6 @@ static void _msm_drm_commit_work_cb(struct kthread_work *work)
 	SDE_ATRACE_END("complete_commit");
 	pm_qos_remove_request(&req);
 
-	end = ktime_get();
-	duration = ktime_to_ns(ktime_sub(end, start));
-	frame_stat_collector(duration, COMMIT_END_TS);
 	if (c->nonblock) {
 		/* Offload the cleanup onto little CPUs (an unbound wq) */
 		INIT_WORK(&c->clean_work, complete_commit_cleanup);
