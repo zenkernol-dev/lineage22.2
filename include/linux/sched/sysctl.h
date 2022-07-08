e943f35c978f9c5de82cb1d7b86e184368e95a27
Alexander Winkowski <dereference23@outlook.com>
2022-07-08 10:26:36 +0000

    sched: Disable cstate checks
    Signed-off-by: Alexander Winkowski <dereference23@outlook.com>

--- include/linux/sched/sysctl.h
+++ include/linux/sched/sysctl.h
@@ -27,7 +27,6 @@ enum { sysctl_hung_task_timeout_secs = 0 };
 extern unsigned int sysctl_sched_latency;
 extern unsigned int sysctl_sched_min_granularity;
 extern unsigned int sysctl_sched_sync_hint_enable;
-extern unsigned int sysctl_sched_cstate_aware;
 extern unsigned int sysctl_sched_wakeup_granularity;
 extern unsigned int sysctl_sched_child_runs_first;
 extern unsigned int sysctl_sched_energy_aware;
@@ -80,6 +79,7 @@ extern unsigned int sysctl_numa_balancing_scan_size;
 
 extern __read_mostly unsigned int sysctl_sched_migration_cost;
 #ifdef CONFIG_SCHED_DEBUG
+extern __read_mostly unsigned int sysctl_sched_cstate_aware;
 extern __read_mostly unsigned int sysctl_sched_nr_migrate;
 extern __read_mostly unsigned int sysctl_sched_time_avg;
 
