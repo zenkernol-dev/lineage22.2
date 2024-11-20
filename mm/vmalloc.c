0dc48208c67390350bd04e1a429871f35fe00b1a
Felix Fietkau <nbd@nbd.name>
2024-11-20 14:59:58 +0800

    debloat: procfs
    Strip non-essential /proc functionality to reduce code size
    
    Imported from openwrt

--- mm/vmalloc.c
+++ mm/vmalloc.c
@@ -2900,7 +2900,11 @@ static const struct file_operations proc_vmalloc_operations = {
 
 static int __init proc_vmalloc_init(void)
 {
-	proc_create("vmallocinfo", S_IRUSR, NULL, &proc_vmalloc_operations);
+
+	if (IS_ENABLED(CONFIG_PROC_STRIPPED))
+		return 0;
+	proc_create("vmallocinfo", 0400, NULL, &proc_vmalloc_operations);
+
 	return 0;
 }
 module_init(proc_vmalloc_init);
