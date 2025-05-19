6c83c9d79dc01a6b2e2e97e1ad033b5584e1e50b
kanger 033 <kanger303@gmail.com>
2025-05-19 08:48:45 +0000

    sched/tune: Introduce SchedTune Assist[v3]
    This implements a mechanism by which default SchedTune parameters
    can be configured in-kernel, circumventing userspace, and
    mitigating reliance on ramdisk modification in the context of
    custom kernels.
    
    [2.5V]: This version adds proper protection
    from userspace (mainly init) trying to write lame
    boost values and gives full control to developer
    and user (sh is not blocked).
    
    [V3.0]: Use a struct to store all the values.
    
    [@0ctobot: Update for msm-4.9 and improve coding style]
    [@YaroST12: Update for msm-4.14]
    Co-authored-by: Adam W. Willis <return.of.octobot@gmail.com>
    Co-authored-by: Yaroslav Furman <yaro330@gmail.com>
    Signed-off-by: Yaroslav Furman <yaro330@gmail.com>
    Signed-off-by: kanger 033 <kanger303@gmail.com>

--- kernel/sched/tune.c
+++ kernel/sched/tune.c
@@ -1,3 +1,4 @@
+#include <linux/binfmts.h>
 #include <linux/cgroup.h>
 #include <linux/err.h>
 #include <linux/kernel.h>
@@ -13,6 +14,10 @@
 
 bool schedtune_initialized = false;
 extern struct reciprocal_value schedtune_spc_rdiv;
+
+#ifdef CONFIG_KPROFILES
+extern int kp_active_mode(void);
+#endif
 
 /* We hold schedtune boost in effect for at least this long */
 #define SCHEDTUNE_BOOST_HOLD_NS 50000000ULL
@@ -555,7 +560,12 @@ int schedtune_task_boost(struct task_struct *p)
 
 	if (unlikely(!schedtune_initialized))
 		return 0;
-
+    
+    #ifdef CONFIG_KPROFILES
+	if (kp_active_mode() == 1)
+		return 0;
+    #endif
+    
 	/* Get task boost value */
 	rcu_read_lock();
 	st = task_schedtune(p);
@@ -572,6 +582,11 @@ int schedtune_prefer_idle(struct task_struct *p)
 
 	if (unlikely(!schedtune_initialized))
 		return 0;
+    
+    #ifdef CONFIG_KPROFILES
+	if (kp_active_mode() == 1)
+		return 0;
+    #endif
 
 	/* Get prefer_idle value */
 	rcu_read_lock();
@@ -655,7 +670,8 @@ int schedtune_prefer_high_cap(struct task_struct *p)
 
 	if (unlikely(!schedtune_initialized))
 		return 0;
-
+    
+    
 	/* Get prefer_high_cap value */
 	rcu_read_lock();
 	st = task_schedtune(p);
@@ -682,6 +698,26 @@ static int prefer_high_cap_write(struct cgroup_subsys_state *css,
 	return 0;
 }
 
+#ifdef CONFIG_STUNE_ASSIST
+static int boost_write_wrapper(struct cgroup_subsys_state *css,
+			       struct cftype *cft, s64 boost)
+{
+	if (task_is_booster(current))
+		return 0;
+
+	return boost_write(css, cft, boost);
+}
+
+static int prefer_idle_write_wrapper(struct cgroup_subsys_state *css,
+				     struct cftype *cft, u64 prefer_idle)
+{
+	if (task_is_booster(current))
+		return 0;
+
+	return prefer_idle_write(css, cft, prefer_idle);
+}
+#endif
+
 static struct cftype files[] = {
 #ifdef CONFIG_SCHED_WALT
 	{
@@ -698,12 +734,20 @@ static struct cftype files[] = {
 	{
 		.name = "boost",
 		.read_s64 = boost_read,
-		.write_s64 = boost_write,
+		#ifdef CONFIG_STUNE_ASSIST
+		.write_s64 = boost_write_wrapper,
+		#else
+		.write_u64 = boost_write,
+        #endif
 	},
 	{
 		.name = "prefer_idle",
 		.read_u64 = prefer_idle_read,
-		.write_u64 = prefer_idle_write,
+		#ifdef CONFIG_STUNE_ASSIST
+		.write_u64 = prefer_idle_write_wrapper,
+		#else
+		.write_u64 = prefer_idle,
+        #endif
 	},
 	{
 		.name = "prefer_high_cap",
@@ -733,6 +777,37 @@ schedtune_boostgroup_init(struct schedtune *st)
 	return 0;
 }
 
+#ifdef CONFIG_STUNE_ASSIST
+struct st_data {
+	char *name;
+	int boost;
+	bool prefer_idle;
+};
+static void write_default_values(struct cgroup_subsys_state *css)
+{
+	static struct st_data st_targets[] = {
+		{ "audio-app",	0, 0 },
+		{ "background",	0, 0 },
+		{ "foreground",	0, 1 },
+		{ "rt",		0, 0 },
+		{ "top-app",	1, 1 },
+	};
+	int i;
+
+	for (i = 0; i < ARRAY_SIZE(st_targets); i++) {
+		struct st_data tgt = st_targets[i];
+
+		if (!strcmp(css->cgroup->kn->name, tgt.name)) {
+			
+			boost_write(css, NULL, tgt.boost);
+			prefer_idle_write(css, NULL, tgt.prefer_idle);
+			pr_info("stune_assist: setting values for %s: boost=%d prefer_idle=%d\n",
+				tgt.name, tgt.boost, tgt.prefer_idle);
+		}
+	}
+}
+#endif
+
 static struct cgroup_subsys_state *
 schedtune_css_alloc(struct cgroup_subsys_state *parent_css)
 {
@@ -748,10 +823,13 @@ schedtune_css_alloc(struct cgroup_subsys_state *parent_css)
 		return ERR_PTR(-ENOMEM);
 	}
 
-	/* Allow only a limited number of boosting groups */
-	for (idx = 1; idx < BOOSTGROUPS_COUNT; ++idx)
+	for (idx = 1; idx < BOOSTGROUPS_COUNT; ++idx) {
 		if (!allocated_group[idx])
 			break;
+#ifdef CONFIG_STUNE_ASSIST
+		write_default_values(&allocated_group[idx]->css);
+#endif
+	}		
 	if (idx == BOOSTGROUPS_COUNT) {
 		pr_err("Trying to create more than %d SchedTune boosting groups\n",
 		       BOOSTGROUPS_COUNT);
